65edab0f8a495062f5f080036c28caf82c8c5c3d
Lukas Stranovsky <lukas.stranovsky@us-spider.com>
2016-10-03 18:55:47 +0200

    [Homework#1] Init Homework#1

---
+++ Homework 1/assets/custom.js
@@ -0,0 +1,27 @@
+$(window).on('scroll', function () {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop != 0) {
+        $('.navbar').stop().animate({
+            'height': '70px',
+            'opacity': '1',
+        }, 'fast');
+        $('.navbar').find('a').stop().animate({'height': '70px'}, 'fast');
+    } else {
+        /*console.log('top reached');*/
+        $('.navbar').stop().animate({
+            'height': '50px',
+            'opacity': '0.7'
+        }, 'fast');
+        $('.navbar').find('a').stop().animate({
+            'height': '50px'
+        }, 'fast');
+    }
+});
+$(window).on('load', function () {
+    $(".se-pre-con").delay(1000).fadeOut("slow");
+});
+$(function () {
+    $('.navbar-brand').on('click', function (e) {
+        e.preventDefault();
+    });
+});
